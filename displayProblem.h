19d7cd531307c5ddf73224394fc6ed73df3ffa34
Iniquez Rusk Jr <iniquezrusk@gmail.com>
2025-11-20 02:18:54 -0600

    displayProblem.h
    Updated MatrixQuestion structure and added rendering functions.

--- displayProblem.h
+++ displayProblem.h
@@ -8,13 +8,22 @@
 
 struct MatrixQuestion {
     int size; // Up to 4
-    int A[4][4];
-    int B[4][4];
-    int correct[4][4];
+    int A[3][3];
+    int B[3];
+    double solution[3];
 };
 
-void randomizeMatrix(MatrixQuestion& question, int n, int range = 3);
+bool solve(MatrixQuestion& q);
 
-bool checkAnswer(const MatrixQuestion& q, const int ans[4][4]);
+void generateSystem(MatrixQuestion& q, int n);
+
+bool solve(MatrixQuestion& q);
+
+bool checkAnswer(MatrixQuestion& q, double userX, double userY, double userZ);
+
+// SDL2 Render text display
+void renderText(SDL_Renderer* renderer, TTF_Font* font, const std::string& text, int x, int y, SDL_Color color);
+void renderMatrixQuestion(SDL_Renderer* renderer, TTF_Font* font, const MatrixQuestion& q, SDL_Color color);
+void renderSolutionLines(SDL_Renderer* renderer, TTF_Font* font, SDL_Color color);
 
 #endif
